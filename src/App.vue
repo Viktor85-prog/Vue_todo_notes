d503b55440d4d3b5b6b5315974aa5b0943520ae8
viktor <viktor.bulkin@stremline-c.com>
2023-01-18 15:39:12 +0300

    create notelist

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,80 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref, onMounted, computed, watch } from 'vue'
+
+const notes = ref([])
+
+const note_input_content = ref('')
+
+
+const notes_asc = computed(()=> notes.value.sort((a,b) => {
+	return b.createdAt - a.createdAt
+}))
+
+const addNote = () => {
+	if(note_input_content.value.trim() === '') {
+		return
+	} 
+	notes.value.push({
+		content:note_input_content.value,
+		done: false,
+		createdAt: new Date().getTime()
+	})
+	note_input_content.value = ''
+}
+
+const removeNote = (note) => {
+	notes.value = notes.value.filter(t => t !== note)
+}
+
+watch(notes, newVal => {
+	localStorage.setItem('notes', JSON.stringify(newVal))
+}, {deep:true})
+
+onMounted(() => {
+	notes.value = JSON.parse(localStorage.getItem('notes')) || []
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <main class="app">
+
+	<section class="greeting">
+		<h2 class="title">
+			Note list
+		</h2>
+	</section>
+
+	<section class="create-todo">
+		<h3>Create note</h3>
+		<form  @submit.prevent="addNote">
+			<h4>What's on your note?</h4>
+			<input  type="text" 
+					placeholder="e.g. home todos"
+					v-model="note_input_content">
+			<input type="submit" value="add note"/>
+		</form>
+	</section>
+
+	<section class="todo-list">
+		<h3>Notes</h3>
+		<div class="list">
+
+			<div :key="note.createdAt"
+				 v-for="note in notes_asc" 
+				:class="`todo-item ${note.done && 'done'}`">
+				<label>
+					<input type="checkbox" v-model="note.done"/>
+				</label>
+				<div class="todo-content">
+					<input type="text" v-model="note.content">
+				</div>
+				<div class="actions">
+					<button class="delete" @click="removeNote(note)">Delete</button>
+				</div>
+			</div>
+		</div>
+	</section>
+
+ </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
